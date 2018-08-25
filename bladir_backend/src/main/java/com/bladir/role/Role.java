438e73a99920d65083c3bd3680d0eb8837f6e1a3
Jerry <sasori.pan@mail.utoronto.ca>
2018-08-24 22:47:58 -0400

    Add basic User and Role class

---
+++ bladir_backend/src/main/java/com/bladir/role/Role.java
@@ -0,0 +1,32 @@
+package com.bladir.role;
+
+public class Role {
+	private int id;
+	private String roleName;
+	
+	public Role() {
+		
+	}
+
+	public Role(int id, String roleName) {
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+}
