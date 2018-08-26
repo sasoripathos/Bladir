782eb73c5d4cdec0cc52e4f8e0349d1b2123d23c
junjie zhang <darren.zhang@mail.utoronto.ca>
2018-08-25 23:33:07 -0400

    Add new Entity Test.

---
+++ bladir_backend/src/main/java/com/bladir/entity/Test.java
@@ -0,0 +1,77 @@
+package com.bladir.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="test")
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="date")
+    private Date date;
+
+    @Column(name="test_comment")
+    private String testcoment;
+
+    @OneToMany(mappedBy="test", fetch=FetchType.LAZY,
+            cascade={CascadeType.DETACH,CascadeType.PERSIST, CascadeType.MERGE,CascadeType.REFRESH})
+    private List<Record> records;
+
+    @ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="patient_id")
+    private User user;
+
+    public Test(Date date, String testcoment, List<Record> records, User user) {
+        this.date = date;
+        this.testcoment = testcoment;
+        this.records = records;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<Record> records) {
+        this.records = records;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTestcoment() {
+        return testcoment;
+    }
+
+    public void setTestcoment(String testcoment) {
+        this.testcoment = testcoment;
+    }
+}
