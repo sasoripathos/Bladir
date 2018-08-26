bf38a88c4cb8a39af3f18b3b4464f37b85c93c35
junjie zhang <darren.zhang@mail.utoronto.ca>
2018-08-25 23:17:59 -0400

    Change the sql script.

--- bladir_backend/src/main/resources/create.sql
+++ bladir_backend/src/main/resources/create.sql
@@ -31,21 +31,34 @@ CREATE TABLE `role` (
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `result`;
-CREATE TABLE `result` (
+
+DROP TABLE IF EXISTS `record`;
+CREATE TABLE `record` (
     `record_id` int(11) NOT NULL AUTO_INCREMENT,
-   `date` TIME DEFAULT NULL,
-   `name` varchar(45) DEFAULT NULL,
-   `test_name` varchar(45) DEFAULT NULL,
-   `patient_id` int(11) DEFAULT NULL,
+   `value_name` varchar(45) DEFAULT NULL,
     `value` NUMERIC(8, 3) DEFAULT NULL,
+   `test_id` int(11) DEFAULT NULL,
+   `record_comment` varchar(45) DEFAULT 'NO COMMNET',
    PRIMARY KEY ( `record_id`),
+    KEY `FK_TEST_idx` (`test_id`),
+    CONSTRAINT `FK_TEST` FOREIGN KEY (`test_id`) 
+    REFERENCES `test` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	KEY `FK_standard_idx` (`value_name`),
+    CONSTRAINT `FK_standard` FOREIGN KEY (`value_name`) 
+    REFERENCES `standard` (`value_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)  ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+   `date` TIME DEFAULT NULL,
+   `test_comment` varchar(45) DEFAULT 'NO COMMNET',
+   `patient_id` int(11) DEFAULT NULL,
+   PRIMARY KEY ( `id`),
     KEY `FK_USER_idx` (`patient_id`),
     CONSTRAINT `FK_USER` FOREIGN KEY (`patient_id`) 
-    REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    KEY `FK_standard_idx` (`test_name`),
-    CONSTRAINT `FK_standard` FOREIGN KEY (`test_name`) 
-    REFERENCES `standard` (`value_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `standard` (
@@ -81,3 +94,15 @@ Insert into user (username, firstname, lastname, password, email, gender, role_i
 
 Insert into user (username, firstname, lastname, password, email, gender, role_id) values
 ('JeanAlen', 'Jean', 'Alen', '12345', 'doctor@email.com', 'female', 1);
+
+Insert into test (date, test_comment, patient_id) values
+(CURRENT_DATE - INTERVAL 1 YEAR, 'You are well',1);
+
+Insert into record (value_name, value, test_id, record_comment) values
+('WBC',11.2, 1,'FEI YAN');
+
+Insert into record (value_name, value, test_id, record_comment) values
+('RBC',5.2, 1,'NORMAL');
+
+Insert into record (value_name, value, test_id, record_comment) values
+('MCV',4.5, 1,'NORMAL');
